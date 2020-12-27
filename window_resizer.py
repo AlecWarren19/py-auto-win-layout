c2fc1a9f747d871dbceb7c5fb2f6383103ce556d
AlecWarren19 <37227576+AlecWarren19@users.noreply.github.com>
2020-12-26 20:08:33 -0500

    Now loads settings from user_config file

--- window_resizer.py
+++ window_resizer.py
@@ -4,61 +4,63 @@ import pyautogui
 from screeninfo import get_monitors
 from jsmin import jsmin
 from time import sleep
+import json
+from win32api import GetMonitorInfo, MonitorFromPoint
+from win32gui import GetWindowRect, FindWindow
+
+CONFIG_FILENAME = "user_config.json"
+
+json_str = ""
+with open(CONFIG_FILENAME) as js_file:
+    json_str = jsmin(js_file.read())
+
+config = json.loads(json_str)
+profile = config["profile"]
+screen_names = config["screen_names"]
+targets = []
 
 monitors = get_monitors()
-#print(monitors)
 
-# # user inputs # #
-targets = ["spotify", "discord"]
-profile = {
-    "spotify" : {"height":0.4, "screen":'l'},
-    "discord" : {"height":0.6, "screen":'l'},
-}
-
-screen_names = ['m', 'l', 'r']
+for target in profile:
+    targets.append(target)
+    if profile[target].get("width"):
+        profile[target]["width"] = eval(profile[target]["width"])
+    else:
+        profile[target]["width"] = 1
+    if profile[target].get("height"):
+        profile[target]["height"] = eval(profile[target]["height"])
+    else:
+        profile[target]["height"] = 1
 screens = {
     screen_names[i]: {
-        "origin": (monitors[i].x, monitors[i].y),
-        "width": None,
-        "height": None,
+        "width": (corners:=GetMonitorInfo(MonitorFromPoint((monitors[i].x + 100, monitors[i].y + 100)))["Work"])[2] - corners[0],
+        "height": corners[3] - corners[1],
+        "origin": corners[:2],
         "occ_w": 0.0,
         "occ_h": 0.0
     } 
     for i in range(len(screen_names))
 }
 
-# takes in a screen dict and a window (Win32Window) object
-def get_screen_info(screen, win):
-
-    win.restore()
-    win.restore()
-    win.resizeTo(100, 100)
-    win.moveTo(screen["origin"][0] + 100, screen["origin"][1] + 100)
-    print("moved win:\n", win)
-    win.maximize()
-    print("max'd win:\n", win)
-    screen["width"] = win.width - 16
-    screen["height"] = win.height - 16
-    screen["origin"] = (win.left + 8, win.top + 8)
-
-# calculate
-get_screen_info(screens[profile[targets[-1]]["screen"]], pygetwindow.getWindowsWithTitle(targets[-1])[0])
-
 for target in targets:
-    while target == "spotify" and not pygetwindow.getWindowsWithTitle(target):
+    wins = pygetwindow.getWindowsWithTitle(target)
+    if target == "spotify" and not wins:
         pyautogui.press("playpause")
-        sleep(.1)
-    if wins:=pygetwindow.getWindowsWithTitle(target):
-        for win in wins:
+        while not (wins:=pygetwindow.getWindowsWithTitle("spotify")):
+            sleep(.01)
+        pyautogui.press("playpause")
+
+    if wins:
+        for win in wins: 
             screen = screens[profile[target]["screen"]]
-            # if this screen's info has not been filled out before
-            if not screen.get("width"):
-                get_screen_info(screen, win)
-                
+                            
+            win.restore()
             win.restore()
             
-            new_w = (int) (screen["width"] * (occ_w:=(1.0 if not "width" in profile[target] else profile[target]["width"])))
-            new_h = (int) (screen["height"] * (occ_h:=(1.0 if not "height" in profile[target] else profile[target]["height"])))
+            #print(GetWindowRect(FindWindow(None, win.title)))
+
+            new_w = (int) (screen["width"] * (occ_w:=profile[target]["width"])) 
+            new_h = (int) (screen["height"] * (occ_h:=profile[target]["height"]))
             win.resizeTo(new_w, new_h)
 
             new_x = screen["origin"][0]
@@ -72,7 +74,7 @@ for target in targets:
             screen["occ_h"] = min(1, screen["occ_h"] + occ_h)
             win.moveTo(new_x, new_y)
             print(target, '\n', win)
-            print(profile[target]["screen"], '\n', screen)
+            #print(profile[target]["screen"], '\n', screen)
 
 """
 print("spotify: \n", pygetwindow.getWindowsWithTitle('spotify')[0])
